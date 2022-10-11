53062ef017c4e52bab9267e8bb8d31fc9a5219e2
Jonty1111 <Jonty.Singla@jda.com>
2022-10-11 10:54:26 +0530

    Initial commit

---
+++ src/main/java/org/example/Arrays/ArrayMethods.java
@@ -0,0 +1,31 @@
+package org.example.testpackage;
+
+public class ArrayMethods {
+    public int findIndex(int[] arr,int num)
+    {
+        int index=-1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==num)
+            {
+                index=i;
+                break;
+            }
+        }
+        return index;
+    }
+
+
+    public boolean searchNumber(int[] arr,int num)
+    {
+        boolean found=false;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==num)
+            {
+                return true;
+            }
+        }
+        return found;
+    }
+}
